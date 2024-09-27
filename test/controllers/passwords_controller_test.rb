79c96a1f01bf46283ef57abe9de7bd9e39287eb7
jokudev <git@joku.dev>
2024-09-27 10:11:51 +0200

    final password controller test

--- test/controllers/passwords_controller_test.rb
+++ test/controllers/passwords_controller_test.rb
@@ -2,24 +2,29 @@ require "test_helper"
 
 class PasswordsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @user = create(:user)
+    @user = users(:one)
   end
 
   test "should get edit" do
-    sign_in_as(@user)
-    get edit_password_url
+    log_in_as(@user)
+    get edit_password_path
     assert_response :success
   end
 
+  test "should not get edit when not logged in" do
+    get edit_password_path
+    assert_redirected_to login_path
+  end
+
   test "should update password" do
-    sign_in_as(@user)
-    patch update_password_url, params: {
-      current_password: "password123",
-      user: {
-        password: "newpassword123",
-        password_confirmation: "newpassword123"
-      }
-    }
+    log_in_as(@user)
+    patch update_password_path, params: {
+                                  user: {
+                                    current_password: "password1234",
+                                    password: "newpassword123",
+                                    password_confirmation: "newpassword123",
+                                  },
+                                }
     assert_redirected_to @user
     # Verify that the password was actually changed
     @user.reload
@@ -27,14 +32,14 @@ class PasswordsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should not update password with incorrect current password" do
-    sign_in_as(@user)
+    log_in_as(@user)
     patch update_password_url, params: {
-      current_password: "wrongpassword",
-      user: {
-        password: "newpassword123",
-        password_confirmation: "newpassword123"
-      }
-    }
+                                 user: {
+                                   current_password: "wrongpassword",
+                                   password: "newpassword1234",
+                                   password_confirmation: "newpassword1234",
+                                 },
+                               }
     assert_response :unprocessable_entity
     # Verify that the password was not changed
     @user.reload
@@ -42,14 +47,14 @@ class PasswordsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should not update password with mismatched confirmation" do
-    sign_in_as(@user)
+    log_in_as(@user)
     patch update_password_url, params: {
-      current_password: "password123",
-      user: {
-        password: "newpassword123",
-        password_confirmation: "differentpassword"
-      }
-    }
+                                 user: {
+                                   current_password: "password1234",
+                                   password: "newpassword1234",
+                                   password_confirmation: "differentpassword",
+                                 },
+                               }
     assert_response :unprocessable_entity
     # Verify that the password was not changed
     @user.reload
@@ -58,7 +63,7 @@ class PasswordsControllerTest < ActionDispatch::IntegrationTest
 
   private
 
-  def sign_in_as(user)
-    post login_url, params: { email: user.email, password: "password123" }
+  def log_in_as(user)
+    post login_path, params: { email: user.email, password: "password1234" }
   end
 end
