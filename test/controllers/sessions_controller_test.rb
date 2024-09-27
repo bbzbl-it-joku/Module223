d0df335201b7d8310103ea4713189a1614e9c98a
jokudev <git@joku.dev>
2024-09-27 10:36:09 +0200

    FINAL SESSION CONTROLLER TEST

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -8,21 +8,31 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create session" do
     user = users(:one)
-    post login_path, params: { email: user.email, password: "password" }
+    post login_path, params: { email: user.email, password: "password1234" }
     assert_redirected_to root_path
-    assert is_logged_in?
+    assert logged_in?
   end
 
   test "should not create session with invalid information" do
     post login_path, params: { email: "", password: "" }
     assert_response :unprocessable_entity
-    assert_not is_logged_in?
+    assert_not logged_in?
   end
 
   test "should destroy session" do
     log_in_as(users(:one))
     delete logout_path
     assert_redirected_to root_path
-    assert_not is_logged_in?
+    assert_not logged_in?
+  end
+
+  private
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def log_in_as(user)
+    post login_path, params: { email: user.email, password: "password1234" }
   end
 end
