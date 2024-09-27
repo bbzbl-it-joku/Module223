2c67924de8db6942b6c59259eca259df3513bad9
jokudev <git@joku.dev>
2024-09-27 09:24:42 +0200

    update user factory and fixtures

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :user do
     sequence(:username) { |n| "user#{n}" }
     sequence(:email) { |n| "user#{n}@example.com" }
-    password { "password123" }
-    password_confirmation { "password123" }
+    password { "password1234" }
+    password_confirmation { "password1234" }
     role { "USER" }
 
     trait :admin do
