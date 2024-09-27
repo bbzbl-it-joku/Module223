ef810e2cb0d8ffb81b02cbc5dfdb8670f170d305
jokudev <git@joku.dev>
2024-09-27 08:28:57 +0200

    final book controller test

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,28 +1,28 @@
 require "test_helper"
 
-class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get login_path
+class BooksControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @book = books(:one)
+    @user = users(:one)
+  end
+
+  test "should get index" do
+    get books_path
     assert_response :success
   end
 
-  test "should create session" do
-    user = users(:one)
+  test "should show book" do
+    get book_path(@book)
+    assert_response :success
+  end
+
+  private
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def log_in_as(user)
     post login_path, params: { email: user.email, password: "password" }
-    assert_redirected_to root_path
-    assert is_logged_in?
-  end
-
-  test "should not create session with invalid information" do
-    post login_path, params: { email: "", password: "" }
-    assert_response :unprocessable_entity
-    assert_not is_logged_in?
-  end
-
-  test "should destroy session" do
-    log_in_as(users(:one))
-    delete logout_path
-    assert_redirected_to root_path
-    assert_not is_logged_in?
   end
 end
